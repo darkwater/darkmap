5d919f6c15d746c490eadfff6c7a124e34b1c9f7
Sam Lakerveld <dark@dark.red>
2024-02-04 00:41:56 +0100

    fix building hover outline

--- darkmap/src/buildings/decorate.rs
+++ darkmap/src/buildings/decorate.rs
@@ -1,10 +1,10 @@
-use std::time::Instant;
+use std::{f32::consts::FRAC_PI_4, time::Instant};
 
 use bevy::{
     prelude::*,
     render::{mesh::Indices, render_resource::PrimitiveTopology},
 };
-use bevy_mod_outline::{OutlineBundle, OutlineMeshExt, OutlineVolume};
+use bevy_mod_outline::{OutlineBundle, OutlineVolume, ATTRIBUTE_OUTLINE_NORMAL};
 use bevy_mod_picking::prelude::*;
 use geo::{Coord, CoordsIter, HaversineBearing, HaversineDistance, LineString, MapCoords, Winding};
 use itertools::Itertools;
@@ -47,7 +47,10 @@ pub fn decorate_building(
         let height = building.tags.building_height().unwrap_or(10.);
 
         let exterior = geometry.exterior_coords_iter().collect::<LineString<f32>>();
-        let exterior = exterior.points_cw().map(Coord::from).collect::<Vec<_>>();
+        let mut exterior = exterior.points_cw().map(Coord::from).collect::<Vec<_>>();
+        if exterior.first() == exterior.last() {
+            exterior.pop();
+        }
 
         // 2d vertices for earcutr
         let vertices = exterior.iter().flat_map(|c| [c.x, c.y]).collect::<Vec<_>>();
@@ -84,6 +87,22 @@ pub fn decorate_building(
             .map(|_| [1., 1., 1., 1.])
             .collect::<Vec<_>>();
 
+        let mut outline_normals = exterior
+            .iter()
+            .map(|c| Vec3::new(c.x, height, c.y))
+            .circular_tuple_windows()
+            .map(|(prev, this, next)| {
+                let prev_angle = (this - prev).normalize();
+                let next_angle = (next - this).normalize();
+                let angle = (prev_angle + next_angle).normalize();
+
+                Quat::from_axis_angle(angle, FRAC_PI_4)
+                    .mul_vec3(Vec3::Y)
+                    .to_array()
+            })
+            .collect::<Vec<_>>();
+        outline_normals.rotate_right(1);
+
         // wall time
         // each wall needs its own set of vertices and normals
         let base = vertices.len() as u32;
@@ -99,6 +118,20 @@ pub fn decorate_building(
                     let normal = (b - a).normalize().cross(Vec3::Y);
                     [[normal.x, normal.y, normal.z]; 4]
                 }),
+        );
+        outline_normals.extend_from_slice(
+            &outline_normals
+                .iter()
+                .circular_tuple_windows()
+                .flat_map(|(a, b)| {
+                    [
+                        [a[0], -a[1], a[2]], //
+                        [a[0], a[1], a[2]],
+                        [b[0], -b[1], b[2]],
+                        [b[0], b[1], b[2]],
+                    ]
+                })
+                .collect_vec(),
         );
         let wall_a = 0.5;
         colors.extend((0..exterior.len() * 4).map(|_| [wall_a, wall_a, wall_a, 1.]));
@@ -119,9 +152,8 @@ pub fn decorate_building(
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
         mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
         mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+        mesh.insert_attribute(ATTRIBUTE_OUTLINE_NORMAL, outline_normals);
         mesh.set_indices(Some(Indices::U32(indices)));
-
-        let _res = mesh.generate_outline_normals();
 
         let mut cmds = commands.entity(entity);
         cmds.insert((
